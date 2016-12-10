bf90169273912646558870c19fce625032fa886f
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-12-09 21:03:43 -0600

    refactor(cleancss): moved common config values to environment variables

--- config/cleancss.config.js
+++ config/cleancss.config.js
@@ -5,11 +5,11 @@ module.exports = {
   /**
    * sourceFileName: the file name of the src css file
    */
-  sourceFileName: 'main.css',
+  sourceFileName: process.env.IONIC_OUTPUT_CSS_FILE_NAME,
 
   /**
    * destFileName: the file name for the generated minified file
    */
-  destFileName: 'main.css'
+  destFileName: process.env.IONIC_OUTPUT_CSS_FILE_NAME
 
 };
