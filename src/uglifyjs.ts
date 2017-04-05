49c0afb1231ce12b6d723b7f1b997d69e665fcb7
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-04-05 11:42:44 -0500

    fix(uglifyjs): better error msg reporting

--- src/uglifyjs.ts
+++ src/uglifyjs.ts
@@ -5,6 +5,7 @@ import * as uglify from 'uglify-js';
 
 import { Logger } from './logger/logger';
 import { fillConfigDefaults, generateContext, getUserConfigFile } from './util/config';
+import { BuildError } from './util/errors';
 import { writeFileAsync } from './util/helpers';
 import { BuildContext, TaskInfo } from './util/interfaces';
 import { runWorker } from './worker-client';
@@ -20,8 +21,8 @@ export function uglifyjs(context: BuildContext, configFile?: string) {
     .then(() => {
       logger.finish();
     })
-    .catch(err => {
-      throw logger.fail(err);
+    .catch((err: BuildError) => {
+      throw logger.fail(new BuildError(err));
     });
 }
 
@@ -53,6 +54,10 @@ export function uglifyjsWorkerImpl(context: BuildContext, uglifyJsConfig: Uglify
     }
 
     return Promise.all(promises);
+  }).catch((err: any) => {
+    // uglify has it's own strange error format
+    const errorString = `${err.message} in ${err.filename} at line ${err.line}, col ${err.col}, pos ${err.pos}`;
+    throw new BuildError(new Error(errorString));
   });
 }
 
