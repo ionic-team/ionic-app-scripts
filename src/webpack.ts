3df19bf4ea1ffc065b7c7cfd7be2371d28e4c30d
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-04-27 11:06:32 -0500

    refactor(webpack): enable throwing errors on webpack build errors

--- src/webpack.ts
+++ src/webpack.ts
@@ -142,7 +142,16 @@ export function runWebpackFullBuild(config: WebpackConfig) {
       if (err) {
         reject(new BuildError(err));
       } else {
-        resolve(stats);
+        const info = stats.toJson();
+
+        if (stats.hasErrors()) {
+          reject(new BuildError(info.errors));
+        } else if (stats.hasWarnings()) {
+          Logger.debug(info.warnings);
+          resolve(stats);
+        } else {
+          resolve(stats);
+        }
       }
     };
     const compiler = webpackApi(config);
