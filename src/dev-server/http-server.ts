2441591ea7db9654c8199130ac963c6992945ed7
Anthony Kuske <anthonykuske@gmail.com>
2017-07-05 22:36:08 +0100

    feature(dev-server): add support for path-based routing
    * feature(dev-server): add fallback route to /index.html to support path based routing
    
    * fix(dev-server): Fix ion-dev css/js when loaded with a path
    
    * remove injector changes

--- src/dev-server/http-server.ts
+++ src/dev-server/http-server.ts
@@ -46,6 +46,9 @@ export function createHttpServer(config: ServeConfig): express.Application {
   app.get('/cordova_plugins.js', servePlatformResource);
   app.get('/plugins/*', servePlatformResource);
 
+  // Fallback route - send to index.html to allow deeplinker to handle path.
+  app.use(serveIndex);
+
   if (config.useProxy) {
     setupProxies(app);
   }
