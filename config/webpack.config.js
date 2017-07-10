feea7fe281a8fa536937e7c26d0325f43abf79c9
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-07-10 13:15:16 -0500

    fix(serve): fix cached file issue by only using the webpack module concat plugin for prod builds, make sure you update custom configs

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,6 +4,11 @@ var ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
 
 var ModuleConcatPlugin = require('webpack/lib/optimize/ModuleConcatenationPlugin');
 
+var prodPlugins = [];
+if (process.env.IONIC_ENV === 'prod') {
+  prodPlugins.push(new ModuleConcatPlugin());
+}
+
 module.exports = {
   entry: process.env.IONIC_APP_ENTRY_POINT,
   output: {
@@ -39,8 +44,7 @@ module.exports = {
   plugins: [
     ionicWebpackFactory.getIonicEnvironmentPlugin(),
     ionicWebpackFactory.getCommonChunksPlugin(),
-    new ModuleConcatPlugin(),
-  ],
+  ].concat(prodPlugins),
 
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
