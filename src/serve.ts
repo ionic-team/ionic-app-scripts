8f71e358ec5fec500d5481d5a166d1bddc711cde
Daniel Imhoff <dwieeb@gmail.com>
2017-08-15 15:54:36 -0500

    fix(serve): fix 'launchBrowser' of undefined
    fixes https://github.com/ionic-team/ionic-cli/issues/2620

--- src/serve.ts
+++ src/serve.ts
@@ -65,7 +65,10 @@ export function serve(context: BuildContext) {
       if (err && err.isFatal) {
         throw err;
       } else {
-        onReady(config, context);
+        if (config) {
+          onReady(config, context);
+        }
+
         return config;
       }
     });
