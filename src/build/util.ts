89292655193b212503ca84a5d53f73e30ede7125
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-09-27 12:49:24 -0500

    fix(build): scan deeplink dir too if different from srcDir

--- src/build/util.ts
+++ src/build/util.ts
@@ -1,11 +1,19 @@
 import { join } from 'path';
+
+import * as Constants from '../util/constants';
 import { GlobResult, globAll } from '../util/glob-util';
-import { readFileAsync } from '../util/helpers';
+import { getStringPropertyValue, readFileAsync } from '../util/helpers';
 import { BuildContext } from '../util/interfaces';
 
 export function scanSrcTsFiles(context: BuildContext) {
-  const tsFileGlob = join(context.srcDir, '**', '*.ts');
-  return globAll([tsFileGlob]).then((results: GlobResult[]) => {
+  const srcGlob = join(context.srcDir, '**', '*.ts');
+  const globs: string[] = [srcGlob];
+  const deepLinkDir = getStringPropertyValue(Constants.ENV_VAR_DEEPLINKS_DIR);
+  // these two will only not be equal in some weird cases like for building Ionic's demos with our current repository set-up
+  if (deepLinkDir !== context.srcDir) {
+    globs.push(join(deepLinkDir, '**', '*.ts'));
+  }
+  return globAll(globs).then((results: GlobResult[]) => {
     const promises = results.map(result => {
       const promise = readFileAsync(result.absolutePath);
       promise.then((fileContent: string) => {
